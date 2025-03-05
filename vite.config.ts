d4f9c13e2d095527efe0cf93a880caca27711198
powerwarez <powerware@naver.com>
2025-03-05 16:25:11 +0900

    빌드 관련 내용 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://stocksimulteacher.netlify.app/'
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
 })
